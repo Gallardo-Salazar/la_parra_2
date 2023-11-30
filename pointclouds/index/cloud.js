4f5bf19f38a0e7211ce963d499d4470bc46c5a20
José Luis Gallardo Salazar <pe_gasa@hotmail.com>
2023-11-29 22:06:44 -0600

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 22086851,
+    "boundingBox": {
+        "lx": 589394.0,
+        "ly": 2651385.25,
+        "lz": 583.7603149414063,
+        "ux": 590333.25,
+        "uy": 2652324.5,
+        "uz": 1523.0103149414063
+    },
+    "tightBoundingBox": {
+        "lx": 589393.9756,
+        "ly": 2651385.1798,
+        "lz": 583.76026171875,
+        "ux": 590333.2481,
+        "uy": 2652103.1327,
+        "uz": 711.92616171875
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 8.134143829345704,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
